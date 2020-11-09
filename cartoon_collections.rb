8c641e4a08a16d7f245e5e5e58c78d3f5abda453
Alexandra Spencer <alexspencer421@gmail.com>
2020-11-09 16:11:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,6 +2,13 @@ def greet_characters(array)
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
+  greeting = ""
+  i = 0 
+  while i < array.each do 
+    p "Hello " ${array[i]} "!"
+  end 
+  i += 1 
+  greeting 
 end
 
 def list_dwarves(array)
